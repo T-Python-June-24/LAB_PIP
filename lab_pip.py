2c7a40b5411d1a98ff7805f9a1cc6ba5f5f3dec4
naif-alharbi22 <naif.n1158@gmail.com>
2024-06-26 08:18:13 +0300

    Solved LAB_PIP & Bonus

--- lab_pip.py
+++ lab_pip.py
@@ -1,4 +1,8 @@
 from art import text2art as font
+from colorama import Fore, Style, init 
+#LAB_PIP
+
+
 
 text1=font("BELIEVE AND ACHEIVE", font='block')
 
@@ -7,3 +11,16 @@ print(text1)
 text2=font("HELLO", font='sub-zero')
 
 print(text2)
+
+
+
+#Bonus
+
+init(autoreset=True)
+
+text3 = font("i love python", font='random')
+print(Fore.GREEN + text3)
+
+
+text4 = font("python", font='small')
+print(Fore.BLUE + text4)
