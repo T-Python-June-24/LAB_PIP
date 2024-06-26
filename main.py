6b164f6ffcb07f8b4717a9ed26e9aa4845b3bec2
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-06-26 06:36:10 +0300

    Completed LAB_PIP with bonus

---
+++ main.py
@@ -0,0 +1,11 @@
+from art import tprint
+from colorama import Fore, Style
+tprint("BELIEVE\nAND\nACHIEVE", font="block")
+tprint("HELLO", font="sub-zero")
+tprint("HELLO", font="random")
+tprint("HELLO", font="spliff")
+tprint("HELLO", font="wavy")
+
+print(Fore.GREEN + Style.DIM + "HELLO")
+print(Fore.GREEN + Style.BRIGHT + "HELLO")
+print(Fore.GREEN + Style.NORMAL + "HELLO")
