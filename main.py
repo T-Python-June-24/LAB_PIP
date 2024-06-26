08b75c135148fc986b2d84c3aeb9b7341e406b92
bushraali98 <bm-bisho-90210@hotmail.com>
2024-06-26 08:07:39 +0300

    Lab solution done

---
+++ main.py
@@ -0,0 +1,9 @@
+from art import text2art
+
+# Printing "BELIEVE AND ACHIEVE"
+phrase1 = text2art("BELIEVE AND ACHIEVE", font='block')
+print(phrase1)
+
+# Printing "HELLO"
+phrase2 = text2art("HELLO", font='sub-zero')
+print(phrase2)
